f7c5f3444661d1b43a89f565ee5fe3c6fdb3ef76
chen <854254943@qq.com>
2018-02-14 21:05:46 +0800

    04 commit

---
+++ LisViewTest/app/src/main/java/com/example/administrator/lisviewtest/Fruit.java
@@ -0,0 +1,25 @@
+package com.example.administrator.lisviewtest;
+
+/**
+ * Created by Administrator on 2018/2/14.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
+
+
